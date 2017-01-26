7a1a6a5e8382b31354c00700cd161326ab10a9de
John Fredrik Asphaug <jfa@noklus.local>
2017-01-26 13:15:52 +0100

    Added more types and stuff

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,63 +1,108 @@
-module Main exposing (..)
 import Html exposing (..)
+import Html.Events exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
+import Random exposing (..)
+import Platform.Cmd exposing (batch)
+import List.Extra exposing(group)
 
--- component import example
-import Components.Hello exposing ( hello )
+import Components.Player as Ply exposing (Player, new, diceView)
 
-
--- APP
-main : Program Never Int Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+  Html.program
+    { init = init "John Fredrik"
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
 
+type Msg 
+ = Roll
+ | NewFace1 Int
+ | NewFace2 Int
+ | NewFace3 Int
+ | NewFace4 Int
+ | NewFace5 Int
 
--- MODEL
-type alias Model = Int
+update : Msg -> Ply.Player -> (Ply.Player, Cmd Msg)
+update msg model = 
+ case msg of 
+  Roll ->
+   (model, batch [ Random.generate NewFace1 (Random.int 1 6)
+                 , Random.generate NewFace2 (Random.int 1 6)
+                 , Random.generate NewFace3 (Random.int 1 6)
+                 , Random.generate NewFace4 (Random.int 1 6)
+                 , Random.generate NewFace5 (Random.int 1 6)])
 
-model : number
-model = 0
+  NewFace1 newFace1 ->
+   ({model | dieFace1 = newFace1}, Cmd.none)
 
+  NewFace2 newFace2 ->
+   ({model | dieFace2 = newFace2}, Cmd.none)
 
--- UPDATE
-type Msg = NoOp | Increment
+  NewFace3 newFace3 ->
+   ({model | dieFace3 = newFace3}, Cmd.none)
 
-update : Msg -> Model -> Model
-update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
+  NewFace4 newFace4 ->
+   ({model | dieFace3 = newFace4}, Cmd.none)
+  
+  NewFace5 newFace5 ->
+   ({model | dieFace5 = newFace5}, Cmd.none)
 
+subscriptions : Ply.Player -> Sub Msg
+subscriptions model =
+  Sub.none
 
--- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
-view : Model -> Html Msg
-view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
-        ]
-      ]
+view : Ply.Player -> Html Msg
+view model = 
+    div []
+    [ (Ply.diceView model)
+    , button [ onClick Roll ] [ text "Roll" ]
+    , div [] [ text (validate model)]
     ]
-  ]
+  
+
+init : String -> (Ply.Player, Cmd Msg)
+init name = 
+    let 
+      player = Ply.new name
+    in
+      (player, Cmd.none)
 
 
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+
+validate: Player -> String
+validate model =
+  let
+    groupedDies = List.Extra.group (List.sort [model.dieFace1, model.dieFace2, model.dieFace3, model.dieFace4, model.dieFace5])
+  in
+    if (List.any isYatzy groupedDies) then
+      "Yatzy"
+    else if (List.any isFourOfaKind groupedDies) then 
+      "Four of a Kind"
+    else if  (List.any isThreeOfaKind groupedDies) && (List.any isPair groupedDies) then
+      "House"
+    else if  (List.any isThreeOfaKind groupedDies) then
+      "Three of a Kind"
+    else if List.length (List.filter isPair groupedDies) == 2 then
+      "Two Pair"
+    else if (List.any isPair groupedDies) then
+      "Pair"
+    else
+      ""
+
+isYatzy : List Int -> Bool
+isYatzy a =
+  List.length a == 5
+
+isFourOfaKind : List Int -> Bool
+isFourOfaKind a =
+  List.length a == 4
+
+isThreeOfaKind : List Int -> Bool
+isThreeOfaKind a =
+  List.length a == 3
+
+
+isPair : List Int -> Bool
+isPair a =
+  List.length a == 2
